cbdeb65ca63c21b824c77d949309d153cfb2d250
crapboy <giulianopertile@gmail.com>
2021-02-05 15:53:40 -0300

    agregado modelo de review y template para mostrarlos

--- books/models.py
+++ books/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 import uuid  # para no usar id en la URL
+from django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
@@ -25,3 +26,19 @@ class Book(models.Model):
 
     def get_absolute_url(self):  # new
         return reverse('book_detail', args=[str(self.id)])
+
+
+class Review(models.Model):  # new
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review
