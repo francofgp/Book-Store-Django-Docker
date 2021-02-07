7ac20682b627ab6290003767cd994b856a82ac90
crapboy <giulianopertile@gmail.com>
2021-02-07 19:59:32 -0300

    agregado busqueda de libros

--- books/urls.py
+++ books/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, SearchResultsListView
 
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
-    
-    path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'), # new
 
+    path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
     # no usamos esto porque en lugar usamos el UUID
     # path('<int:pk>', BookDetailView.as_view(), name='book_detail'),  # new
 ]
--- books/views.py
+++ books/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.mixins import (
 from django.views.generic import ListView, DetailView
 
 from .models import Book
+from django.db.models import Q
 
 
 class BookListView(LoginRequiredMixin, ListView):
@@ -23,3 +24,20 @@ class BookDetailView(
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):  # new
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    #queryset = Book.objects.filter(title__icontains='dracula')
+    # Asi sobreescribimos el queryset para tener mas flexibilidad
+    # "|" significa OR"
+    def get_queryset(self):
+        # Esa "q" esta en el HTML  y es el name del input
+        # de la busqueda
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
